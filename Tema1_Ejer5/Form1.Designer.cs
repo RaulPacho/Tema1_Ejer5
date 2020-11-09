1dea0dbeec0d64751bce7d51370e442695d93a2d
Raul Pacho <raulpachovivas@gmail.com>
2020-11-09 16:40:39 +0100

    Agregar archivos de proyecto.

---
+++ Tema1_Ejer5/Form1.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace Tema1_Ejer5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.ForeColor = System.Drawing.Color.DimGray;
+            this.textBox1.Location = new System.Drawing.Point(254, 29);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(233, 124);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "Escribe aqui tu texto...";
+            this.textBox1.TextChanged += new System.EventHandler(this.TextBox1_TextChanged);
+            this.textBox1.Enter += new System.EventHandler(this.TextBox1_Enter);
+            this.textBox1.Leave += new System.EventHandler(this.TextBox1_Leave);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(322, 209);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Enviar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Tema1_Ejer5/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tema1_Ejer5
+{
+    public partial class Form1 : Form
+    {
+        string aux = "";
+        bool escrito = false;
+        public Form1()
+        {
+            InitializeComponent();
+            this.ActiveControl = button1;
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TextBox1_Enter(object sender, EventArgs e)
+        {
+            textBox1.Text = aux;
+            textBox1.ForeColor = Color.Black;
+            escrito = true;
+        }
+
+        private void TextBox1_Leave(object sender, EventArgs e)
+        {
+            if(textBox1.Text.Trim() == "")
+            {
+
+                textBox1.Text = "Escribe aqui tu texto...";
+                escrito = false;
+                textBox1.ForeColor = Color.DimGray;
+            }
+            else
+            {
+                aux = textBox1.Text;
+            }
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            /*string text,
+            string caption,
+            MessageBoxButtons buttons,
+            MessageBoxIcon icon,
+            MessageBoxDefaultButton defaultButton*/
+            DialogResult a;
+            if (escrito)
+            {
+                a = MessageBox.Show("¿Quieres establecer \"" + textBox1.Text + "\" como titulo?", "Titulo", MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button1);
+
+                if (a == DialogResult.Yes)
+                {
+                    this.Text = textBox1.Text;
+                }
+            }
+            else
+            {
+                a = MessageBox.Show("No has escrito na", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error, MessageBoxDefaultButton.Button1);
+
+            }
+        }
+    }
+}
